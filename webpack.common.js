90ff761235ce958c8fa4acafadf5ab44296d3e10
pjoterini <piotrekjan.gorski@gmail.com>
2023-08-12 11:17:29 +0200

    arch portfolio newest

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    bundle: path.resolve(__dirname, "src/index.js"),
+    bundle: path.resolve(__dirname, 'src/index.js')
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name][contenthash].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
     clean: true,
-    assetModuleFilename: "assets/[name][ext]",
+    assetModuleFilename: 'assets/[name][ext]'
   },
 
   module: {
@@ -18,43 +18,43 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|webp|mkv|pdf|mp4)$/i,
-        type: "asset/resource",
-      },
-    ],
+        type: 'asset/resource'
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "PiotrGórskiPortfolio",
-      filename: "index.html",
-      template: "src/template.html",
+      title: 'PiotrGórski',
+      filename: 'index.html',
+      template: 'src/template.html'
     }),
     new HtmlWebpackPlugin({
-      title: "PiotrGórskiPortfolio",
-      filename: "thankyou.html",
-      template: "src/pages/thankyou.html",
+      title: 'PiotrGórski',
+      filename: 'thankyou.html',
+      template: 'src/pages/thankyou.html'
     }),
     new HtmlWebpackPlugin({
-      title: "PiotrGórskiPortfolio",
-      filename: "more-about-me.html",
-      template: "src/pages/more-about-me.html",
-    }),
+      title: 'PiotrGórski',
+      filename: 'more-about-me.html',
+      template: 'src/pages/more-about-me.html'
+    })
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, 'dist')
     },
     compress: true,
     port: 3000,
     open: true,
     hot: true,
-    historyApiFallback: true,
-  },
-};
+    historyApiFallback: true
+  }
+}
